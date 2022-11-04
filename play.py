084670cf3b9f422569bc8827f1552a0ff9a1b26c
Sanjib Katuwal <katsanjib046@gmail.com>
2022-11-04 19:38:01 -0400

    Removed the bugs and added readme

--- play.py
+++ play.py
@@ -4,6 +4,7 @@ import time
 import random
 import nim
 
+
 # Initialize pygame
 pygame.init()
 size = width, height = 1000, 800
@@ -17,26 +18,36 @@ WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
+ORANGE = (255, 165, 0)
+SKY_BLUE = (135, 206, 235)
+GRAY = (128, 128, 128)
+TORQUOISE = (64, 224, 208)
 
 # Define some fonts
+smallFont = pygame.font.SysFont("comicsansms", 15)
 mediumFont = pygame.font.Font("OpenSans-Regular.ttf", 24)
 mediumFont.set_bold(True)
-largeFont = pygame.font.Font("OpenSans-Regular.ttf", 32)
+largeFont = pygame.font.Font("OpenSans-Regular.ttf", 64)
 largeFont.set_bold(True)
-
-# # Define some images
-# pile_image = pygame.image.load("pile.png")
+extraLargeFont = pygame.font.Font("OpenSans-Regular.ttf", 94)
+extraLargeFont.set_bold(True)
 
 # Define some sounds
 click_sound = pygame.mixer.Sound("click.wav")
 winner = pygame.mixer.Sound("winner.wav")
 loser = pygame.mixer.Sound("loser.wav")
+# background music
+pygame.mixer.music.load("background.ogg")
+# play the background music indefinitely
+pygame.mixer.music.play(-1)
 
 
+# some variables to handle functions
 running = True
 level = False
 gameOn = False
-
+p = None
+c= None
 while running:
     # Handle events
     for event in pygame.event.get():
@@ -45,35 +56,42 @@ while running:
             sys.exit()
 
     # Fill the screen with white
-    screen.fill(WHITE)
+    screen.fill(SKY_BLUE)
 
     if not gameOn:
 
         # Draw the title
-        title = mediumFont.render("Play Nim", True, GREEN)
+        title = extraLargeFont.render("Play Nim With AI", True, ORANGE)
         titleRect = title.get_rect()
-        titleRect.center = (width // 2, height // 2 - 100)
+        titleRect.center = (width // 2, height // 2 - 300)
         screen.blit(title, titleRect)
 
         if level == False:
             # Select the difficulty level
-            easyButton = pygame.draw.rect(screen, GREEN, (width // 2 - 100, height // 2 - 50, 200, 50))
+            easyButton = pygame.draw.rect(screen, TORQUOISE, (width // 2 - 100, height // 2 - 55, 200, 50))
             easyText = mediumFont.render("Easy", True, WHITE)
             easyTextRect = easyText.get_rect()
             easyTextRect.center = (width // 2, height // 2 - 25)
             screen.blit(easyText, easyTextRect)
 
-            mediumButton = pygame.draw.rect(screen, GREEN, (width // 2 - 100, height // 2, 200, 50))
+            mediumButton = pygame.draw.rect(screen, ORANGE, (width // 2 - 100, height // 2, 200, 50))
             mediumText = mediumFont.render("Medium", True, WHITE)
             mediumTextRect = mediumText.get_rect()
             mediumTextRect.center = (width // 2, height // 2 + 25)
             screen.blit(mediumText, mediumTextRect)
 
-            hardButton = pygame.draw.rect(screen, GREEN, (width // 2 - 100, height // 2 + 50, 200, 50))
+            hardButton = pygame.draw.rect(screen, RED, (width // 2 - 100, height // 2 + 55, 200, 50))
             hardText = mediumFont.render("Hard", True, WHITE)
             hardTextRect = hardText.get_rect()
             hardTextRect.center = (width // 2, height // 2 + 75)
             screen.blit(hardText, hardTextRect)
+
+            # add a quit button
+            quitButton = pygame.draw.rect(screen, GRAY, (width // 2 - 100, height // 2 + 200, 200, 50))
+            quitText = mediumFont.render("Quit", True, WHITE)
+            quitTextRect = quitText.get_rect()
+            quitTextRect.center = (width // 2, height // 2 + 225)
+            screen.blit(quitText, quitTextRect)
 
             # Add the event for these buttons
             if event.type == pygame.MOUSEBUTTONDOWN:
@@ -92,20 +110,31 @@ while running:
                     train_n = 10000
                     level = True
                     time.sleep(0.5)
-            
+                elif quitButton.collidepoint(event.pos):
+                    click_sound.play()
+                    running = False
+                    sys.exit()
+
+                # leave a note that the AI is training
+                if level:
+                    trainText = mediumFont.render("AI is training...", True, WHITE)
+                    trainTextRect = trainText.get_rect()
+                    trainTextRect.center = (width // 2, height // 2 + 150)
+                    screen.blit(trainText, trainTextRect)
+                    pygame.display.update()
                 # Train the AI
                 ai = nim.train(train_n)
 
         else:
             # Draw the buttons
-            playButton = pygame.draw.rect(screen, GREEN, (width // 2 - 100, height // 2 - 50, 200, 50))
-            playText = mediumFont.render("Start", True, WHITE)
+            playButton = pygame.draw.rect(screen, TORQUOISE, (width // 2 - 100, height // 2 - 50, 200, 50))
+            playText = mediumFont.render("Start", True, ORANGE)
             playTextRect = playText.get_rect()
             playTextRect.center = (width // 2, height // 2 - 25)
             screen.blit(playText, playTextRect)
 
-            backButton = pygame.draw.rect(screen, GREEN, (width // 2 - 100, height // 2 + 50, 200, 50))
-            backText = mediumFont.render("Back", True, WHITE)
+            backButton = pygame.draw.rect(screen, GRAY, (width // 2 - 100, height // 2 + 50, 200, 50))
+            backText = mediumFont.render("Back", True, BLACK)
             backTextRect = backText.get_rect()
             backTextRect.center = (width // 2, height // 2 + 75)
             screen.blit(backText, backTextRect)
@@ -129,20 +158,8 @@ while running:
 
     else:
         if game.winner is None:
-
-            # print whose turn it is
-            if game.player == human_player:
-                # show that it is the human player's turn at the bottom of the screen
-                playerText = mediumFont.render("Your turn", True, BLACK)
-                playerTextRect = playerText.get_rect()
-                playerTextRect.center = (width // 2, height - 50)
-                screen.blit(playerText, playerTextRect)
-            else:
-                # show that it is the AI's turn at the bottom of the screen
-                playerText = mediumFont.render("AI's turn", True, BLACK)
-                playerTextRect = playerText.get_rect()
-                playerTextRect.center = (width // 2, height - 50)
-                screen.blit(playerText, playerTextRect)
+            # empty dict for buttons
+            button = {}
 
             # get all the actions
             actions = nim.Nim.available_actions(game.piles)
@@ -150,40 +167,77 @@ while running:
             # Draw the piles with a loop
             for i in range(len(game.piles)):
                 pile = game.piles[i]
-                # draw small circles for each pile
-                for j in range(pile):
-                    pygame.draw.circle(screen, BLACK, (100 + i * 200, 100 + j * 50), 20)
                 
-                # print an info text
+                # print an info text for each pile
                 pileText = mediumFont.render("Pile " + str(i + 1), True, BLACK)
                 pileTextRect = pileText.get_rect()
                 pileTextRect.center = (100 + i * 200, 50)
                 screen.blit(pileText, pileTextRect)
-
+                
                 # Draw small buttons for each pile at the bottom of the pile
                 for j in range(pile):
                     # buttons for the human player
-                    button = pygame.draw.circle(screen, GREEN, (100 + i * 200, 100 + j * 50), 20, 1)
+                    button[str(i)+str(j)] = pygame.draw.circle(screen, GREEN, (100 + i * 200, 100 + j * 50), 20)
                     # show the number of stones to remove
                     buttonText = mediumFont.render(str(j + 1), True, WHITE)
                     buttonTextRect = buttonText.get_rect()
                     buttonTextRect.center = (100 + i * 200, 100 + j * 50)
                     screen.blit(buttonText, buttonTextRect)
 
-                    # Add the event for these buttons if it's the human player's turn
-                    if game.player == human_player:
+            # Add instructions on how to play
+            instructionText = smallFont.render("Instruction: Click on a number to remove that many stones from that pile.", True, BLACK)
+            instructionTextRect = instructionText.get_rect()
+            instructionTextRect.center = (width // 2, height - 300)
+            screen.blit(instructionText, instructionTextRect)
+            instructionText2 = smallFont.render("The player who removes the last stone loses.", True, BLACK)
+            instructionTextRect2 = instructionText2.get_rect()
+            instructionTextRect2.center = (width // 2, height - 280)
+            screen.blit(instructionText2, instructionTextRect2)
+
+
+
+            
+            # Add the event for these buttons if it's the human player's turn
+            if game.player == human_player:
+                # print an info that it is the human player's turn
+                turnText = mediumFont.render("Your turn", True, BLACK)
+                turnTextRect = turnText.get_rect()
+                turnTextRect.center = (width // 2, height - 50)
+                screen.blit(turnText, turnTextRect)
+                # print AI's previous move
+                if p is not None and c is not None:
+                    word = 'stone' if c == 1 else 'stones'
+                    aiText = mediumFont.render("AI removed " + str(c) + " "+ word +" from pile " + str(p + 1), True, BLACK)
+                    aiTextRect = aiText.get_rect()
+                    aiTextRect.center = (width // 2, height - 100)
+                    screen.blit(aiText, aiTextRect)
+                    pygame.display.update()
+                for i in range(len(game.piles)):
+                    pile = game.piles[i]
+                    for j in range(pile):
                         if event.type == pygame.MOUSEBUTTONDOWN:
-                            if button.collidepoint(event.pos):
+                            if button[str(i)+str(j)].collidepoint(event.pos):
                                 click_sound.play()
                                 game.move((i, j + 1))
-                    # else if it's the AI's turn, let the AI play and wait for 1 second
-                    elif game.player != human_player:
-                        time.sleep(2)
-                        p,c = ai.choose_action(game.piles)
-                        game.move((p, c))
+            # else if it's the AI's turn, let the AI play and wait for 1 second
+            else:
+                # print that it is the AI's turn and wait for 1 second
+                aiText = mediumFont.render("AI is thinking...", True, BLACK)
+                aiTextRect = aiText.get_rect()
+                aiTextRect.center = (width // 2, height - 100)
+                screen.blit(aiText, aiTextRect)
+                pygame.display.update()
+                time.sleep(1)
+                # let the AI play
+                p,c = ai.choose_action(game.piles)
+                game.move((p, c))
+                        
         
-        # check if the game is over
+        # if the game is over
         else:
+            # reset p and c
+            p = None
+            c = None
             # show the winner at the top of the screen
             if game.winner == human_player:
                 winnerText = largeFont.render("You won!", True, GREEN)
@@ -197,8 +251,8 @@ while running:
             sound.play()
 
             # show the back button
-            backButton = pygame.draw.rect(screen, GREEN, (width // 2 - 100, height // 2 + 50, 200, 50))
-            backText = mediumFont.render("Back", True, WHITE)
+            backButton = pygame.draw.rect(screen, TORQUOISE, (width // 2 - 100, height // 2 + 50, 200, 50))
+            backText = mediumFont.render("REPLAY", True, WHITE)
             backTextRect = backText.get_rect()
             backTextRect.center = (width // 2, height // 2 + 75)
             screen.blit(backText, backTextRect)
@@ -210,9 +264,20 @@ while running:
                     gameOn = False
                     level = False
                     time.sleep(0.5)
+            # button to quit the game
+            quitButton = pygame.draw.rect(screen, GRAY, (width // 2 - 100, height // 2 + 150, 200, 50))
+            quitText = mediumFont.render("Quit", True, WHITE)
+            quitTextRect = quitText.get_rect()
+            quitTextRect.center = (width // 2, height // 2 + 175)
+            screen.blit(quitText, quitTextRect)
+            # add the event for this button
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if quitButton.collidepoint(event.pos):
+                    click_sound.play()
+                    running = False
+                    sys.exit()
 
     pygame.display.flip()
-
 
 pygame.quit()
 
--- readme.md
+++ readme.md
@@ -1,2 +1,49 @@
 # Nim: Play Against AI
-#### This is a Nim game that you can play against an AI. The AI uses the reinforcement learning algorithm Q-learning to learn how to play the game.
+
+### By Sanjib Katuwal
+University of Florida
+
+Gainesville, FL
+USA
+
+### [Video Demo](https://youtu.be/ybxPolsjZ6Q)
+## Introduction
+
+### What is Nim?
+Nim is a mathematical game of strategy in which two players take turns removing objects from distinct heaps. On each turn, a player must remove at least one object, and may remove any number of objects provided they all come from the same heap. The goal of the game is to avoid taking the last object. 
+
+### What is Nim AI?
+This is a Nim game that you can play against an AI. The AI uses the reinforcement learning algorithm Q-learning to learn how to play the game.
+
+### What is Q-learning?
+Q-learning is a model-free reinforcement learning algorithm. It learns a policy, which tells an agent what action to take under what circumstances. Q-learning can be used to find an optimal action-selection policy for any given (finite) Markov decision process (MDP).
+
+## How to Play
+The following are the rules of the game:
+1. There are three different levels of difficulty: easy, medium, and hard.
+2. There are 4 heaps of objects. Each heap has a different number of objects. The number of objects in the first pile is 1, the second pile is 3, the third pile is 5, and the fourth pile is 7.
+3. The player and the AI will take turns removing objects from the piles. Who will go first is randomly decided.
+4. When it is your turn, you will see "Your turn" at the bottom of the screen. You can click on the piles to remove objects from them. You can remove any number of objects from a pile, but you must remove at least one object. If you click on number 5 from pile 4 then you will remove 3 objects from pile 4.
+6. The AI will take its turn after you. You will see "AI is thinking..." at the bottom of the screen.
+7. The game ends when there are no more objects left in the piles. The player who removes the last object loses. So try not to remove the last object!
+
+## How to Run
+In your terminal, run the following command:
+```python play.py```
+
+## Description of Files
+1. nim.py: This file contains the Nim class, NimAI class and training function. It also has play function if you want to play directly using the terminal as in CS50AI project. Class Nim contains the game logic. It initiates the board for the game and make moves and checks if the game is over. Class NimAI contains the AI logic. It uses the Q-learning algorithm to learn how to play the game. The train function trains the AI for some number of games. The play function is used to play the game directly in the terminal.
+2. play.py: It uses the pygame library to create the game interface. If you choose easy level, the AI will be trained for 100 games. If you choose medium level, the AI will be trained for 1000 games. If you choose hard level, the AI will be trained for 10000 games. 
+3. click.wav: This is the sound effect when you click on a button or a pile.
+4. winner.wav: This is the sound effect when you win the game.
+5. loser.wav: This is the sound effect when you lose the game.
+6. background.ogg: This is the background music.
+7. OpenSans-Regular.ttf: This is the font used in the game.
+8. README.md: This is the readme file.
+9. requirements.txt: This file contains the list of libraries that you need to install to run the game. It need only one module, pygame. You can install it by running the following command in your terminal: ```pip install pygame```
+
+## References
+1. [Reinforcement Learning](https://en.wikipedia.org/wiki/Reinforcement_learning)
+2. [CS50AI](https://cs50.harvard.edu/ai/2020)
+3. [Q-learning](https://en.wikipedia.org/wiki/Q-learning)
+4. [Nim](https://en.wikipedia.org/wiki/Nim)
